e2969efe760502151ccb42f7257e5bb11ec7fbb0
Kirill Stativka <ssko_17@mail.ru>
2017-05-09 22:30:51 +0300

    Some Fix

--- marcup/js/common.js
+++ marcup/js/common.js
@@ -1,23 +1,23 @@
-$(function() {
+$(function () {
 
 	//SVG Fallback
-	if(!Modernizr.svg) {
-		$("img[src*='svg']").attr("src", function() {
+	if (!Modernizr.svg) {
+		$("img[src*='svg']").attr("src", function () {
 			return $(this).attr("src").replace(".svg", ".png");
 		});
 	};
 
 	//E-mail Ajax Send
 	//Documentation & Example: https://github.com/agragregra/uniMail
-	$("form").submit(function() { //Change
+	$("form").submit(function () { //Change
 		var th = $(this);
 		$.ajax({
 			type: "POST",
 			url: "mail.php", //Change
 			data: th.serialize()
-		}).done(function() {
+		}).done(function () {
 			alert("Thank you!");
-			setTimeout(function() {
+			setTimeout(function () {
 				// Done Functions
 				th.trigger("reset");
 			}, 1000);
@@ -28,29 +28,44 @@ $(function() {
 	//Chrome Smooth Scroll
 	try {
 		$.browserSelector();
-		if($("html").hasClass("chrome")) {
+		if ($("html").hasClass("chrome")) {
 			$.smoothScroll();
 		}
-	} catch(err) {
+	} catch (err) {
 
 	};
 
-	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
-	
+	$("img, a").on("dragstart", function (event) { event.preventDefault(); });
+
 });
 
-$(window).load(function() {
+$(window).load(function () {
 
 	$(".loader_inner").fadeOut();
 	$(".loader").delay(400).fadeOut("slow");
 
 });
 $(".slider_wrap").owlCarousel({
-		autoPlay: 5000,
-		navigation: true,
-		slideSpeed: 300,
-		paginationSpeed: 400,
-		singleItem: true,
-		items: 1,
-		navigationText: ['<span class="prev_button"></span>', '<span class="next_button"></span>']
-	});
+	autoPlay: 5000,
+	navigation: true,
+	slideSpeed: 300,
+	paginationSpeed: 400,
+	singleItem: true,
+	items: 1,
+	navigationText: ['<span class="prev_button"></span>', '<span class="next_button"></span>']
+});
+$(window).scroll(function () {
+
+	if ($('body').scrollTop() > 100) {
+		$('header').addClass('navbar_fixed_top');
+	} else {
+		$('header').removeClass('navbar_fixed_top');
+	}
+
+});
+
+$('a[href^="#"]').click(function () {
+	var target = $(this).attr('href');
+	$('html, body').animate({ scrollTop: $(target).offset().top }, 800);
+	return false;
+});
